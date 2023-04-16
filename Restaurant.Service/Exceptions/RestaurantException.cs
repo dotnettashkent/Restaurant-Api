6bdb6f0198ad034d8930f6bb00e05600ba909cb6
Saeed <dotnettashkent@gmail.com>
2023-04-17 01:01:46 +0500

    Added EmailVerification and PasswordHasher

--- Restaurant.Service/Exceptions/RestaurantException.cs
+++ Restaurant.Service/Exceptions/RestaurantException.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Restaurant.Service.Exceptions
+﻿namespace Restaurant.Service.Exceptions
 {
     public class RestaurantException : Exception
     {
         public int Code { get; set; }
 
-        public RestaurantException(int code,string message)
+        public RestaurantException(int code, string message)
             : base(message)
         {
             this.Code = code;
