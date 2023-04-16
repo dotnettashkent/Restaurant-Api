cd0e89829e10bda9e226959bac302ca0355abae3
Saeed <dotnettashkent@gmail.com>
2023-04-16 12:46:59 +0500

    Added other items

--- Restaurant.Service/DTOs/Users/UserDto.cs
+++ Restaurant.Service/DTOs/Users/UserDto.cs
@@ -8,5 +8,9 @@ namespace Restaurant.Service.DTOs.Users
 {
     public class UserDto
     {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
     }
 }
